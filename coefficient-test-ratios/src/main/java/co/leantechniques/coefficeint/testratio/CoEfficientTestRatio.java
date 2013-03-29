53bbb994de582af107b3df1dbb75912f7cb8ec3f
Brandon Carlson <bcarlso@gmail.com>
2013-03-28 22:26:20 -0500

    Added command line processing

--- coefficient-test-ratios/src/main/java/co/leantechniques/coefficeint/testratio/CoEfficientTestRatio.java
+++ coefficient-test-ratios/src/main/java/co/leantechniques/coefficeint/testratio/CoEfficientTestRatio.java
@@ -2,23 +2,43 @@ package co.leantechniques.coefficeint.testratio;
 
 import co.leantechniques.coefficient.scm.CodeRepositoryFactory;
 import co.leantechniques.coefficient.scm.WorkingDirectory;
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.ParameterException;
 
 public class CoEfficientTestRatio implements TestRatioListener {
+    @Parameter(names = {"--limit"}, description = "Number of days to go back through history")
+    private int dailyLimit = 365;
+
+    @Parameter(names = {"--bare"}, description = "Output the ratio in a format suitable for piping")
+    private boolean bare = false;
+
     public static void main(String[] args) {
-        CoEfficientTestRatio coEfficientTestRatio = new CoEfficientTestRatio();
         CodeRepositoryFactory codeRepositoryFactory = new CodeRepositoryFactory();
+        CoEfficientTestRatio coEfficientTestRatio = new CoEfficientTestRatio();
+        JCommander jc = new JCommander(coEfficientTestRatio);
 
-        TestRatio ratio = new TestRatio();
-        int pastDaysLimit = 365;
-        System.out.println("args[0] = " + args[0]);
-        ratio.setCodeRepository(codeRepositoryFactory.build(new WorkingDirectory("."), pastDaysLimit));
-        ratio.setTestRatioListener(coEfficientTestRatio);
+        try {
+            jc.parse(args);
 
-        ratio.calculate();
+            TestRatio ratio = new TestRatio();
+            ratio.setCodeRepository(codeRepositoryFactory.build(new WorkingDirectory("."), coEfficientTestRatio.dailyLimit));
+            ratio.setTestRatioListener(coEfficientTestRatio);
+
+            ratio.calculate();
+        } catch (ParameterException e) {
+            jc.setProgramName("coefficient-test-ratio");
+            jc.usage();
+        } catch (UnsupportedOperationException e) {
+            System.out.println("Unable to determine repository type. Are you in the proper directory?");
+        }
     }
 
     @Override
     public void testRatioCalculated(int percentOfCommitsWithTests) {
-        System.out.println(percentOfCommitsWithTests);
+        if (bare)
+            System.out.println(percentOfCommitsWithTests);
+        else
+            System.out.println("Over the last " + dailyLimit + " days, " + percentOfCommitsWithTests + "% of commits contain tests");
     }
 }
